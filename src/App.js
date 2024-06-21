ef3768febc7cc566345a414c4a773d3e68bad727
Yash020405 <yashaga0204@gmail.com>
2024-06-21 10:03:47 +0530

    Cart_Context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import Products from './components/Products/Products';
 import { useState } from 'react';
+import CartContext from './context/CartContext';
 function App() {
-  // state variable
-  // inc
-  // dec
   let [cart, setCart] = useState({});
   function increaseQuantity(product) {
     const newCart = { ...cart };
-    // if(cart[product.id])
     if (!newCart[product.id]) {
       newCart[product.id] = {
         ...product,
@@ -31,22 +28,12 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Products cart={cart} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} />
-    </div>
+    <CartContext.Provider value={{ cart, increaseQuantity, decreaseQuantity }}>
+      <div className="App">
+        <Products cart={cart} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} />
+      </div>
+    </CartContext.Provider>
   );
 }
 
 export default App;
-
-// {1:{id: 1, quantity: 11}, 2:{id: 2, quantity: 10}, 3:{id: 3, quantity: 10}, 4:{id: 4, quantity: 10}}
-
-// button
-// - 1 +
-
-// let a = {b:10, c:20};
-//a.b = 30;
-// let b = {...a};
-
-// a => 1234
-// a => 12
--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -1,6 +1,7 @@
+import { useContext } from "react";
+import CartContext from "../../context/CartContext";
 function AddToCart({ product,  cart, increaseQuantity, decreaseQuantity }) {
-    // inc
-    // dec
+    const { cart, increaseQuantity, decreaseQuantity } = useContext(CartContext);
     function increase() {
         increaseQuantity(product);
     }
